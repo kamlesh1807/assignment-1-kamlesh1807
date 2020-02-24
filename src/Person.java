7499ec295563cdb214cca756ae77769dc25c13a1
KAMLESH <kamlesh.yadav_cs18@gla.ac.in>
2020-02-24 21:02:07 +0530

    create and define Person class

---
+++ src/Person.java
@@ -0,0 +1,67 @@
+import java.util.ArrayList;
+
+public class Person {
+    private String first;
+    private String last;
+    private String temp;
+    private String emai;
+    private ArrayList<String> numbers = new ArrayList<>();
+
+    public String getFirst() {
+        return first;
+    }
+
+    public void setFirst(String first) {
+        this.first = first;
+    }
+
+    public String getLast() {
+        return last;
+    }
+
+    public void setLast(String last) {
+        this.last = last;
+    }
+
+    public String getEmai() {
+        return emai;
+    }
+
+    public void setEmai(String emai) {
+        this.emai = emai;
+    }
+
+    public void setNumber(String n) {
+        numbers.add(n);
+    }
+
+    public void showNumber() {
+        if (numbers.size() > 1) {
+            System.out.print("Contact Number(s): ");
+        } else {
+            System.out.print("Contact Number: ");
+        }
+        for (int i = 0; i < numbers.size(); i++) {
+            System.out.print(numbers.get(i));
+            if (i != numbers.size() - 1) {
+                System.out.print(", ");
+            }
+        }
+    }
+
+    public void showPerson() {
+        System.out.println("-------- * Here are all your Contacts * --------");
+        System.out.println("First Name: " + first);
+        System.out.println("Last Name: " + last);
+        showNumber();
+        System.out.println("\n" + "Email address: " + emai);
+        System.out.println("-------- * -------- * -------- * --------");
+        System.out.println("-------- * -------- * -------- * --------");
+    }
+
+    @Override
+    public String toString() {
+        return first;
+    }
+
+}
